c5276f8d72d9010d512a49a38ade20ee36d49055
Jeffrey Schmidt <jschmid1@binghamton.edu>
2015-03-09 13:40:18 -0400

    Fixed code so the drop down box recognized the drug being sent to the textbox.

--- Drug interaction NETWORK SCIENCE SELENIUM.py
+++ Drug interaction NETWORK SCIENCE SELENIUM.py
@@ -5,15 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+DRUG_ONE = "codeine"
+DRUG_TWO = "Adderall"
+
 browser = webdriver.Firefox()
 browser.get('http://reference.medscape.com/drug-interactionchecker')
 
 search = browser.find_element_by_id('MDICtextbox')
-search.send_keys("codeine")
+for char in DRUG_ONE:    
+    search.send_keys(char)
+time.sleep(2)
 search.send_keys(Keys.RETURN) # hit return after you enter search text
 
 search = browser.find_element_by_id('MDICtextbox')
-search.send_keys("adderall")
+for char in DRUG_TWO:
+    search.send_keys(char)
+time.sleep(2)
 search.send_keys(Keys.RETURN) # hit return after you enter search text
 time.sleep(5) # sleep for 5 seconds so you can see the results
 browser.quit()
